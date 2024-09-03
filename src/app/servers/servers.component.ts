6e94b61e73edb3c4829119b10a4b2107acfa3078
ArthurZPC <guseynoff.artur@yandex.ru>
2024-09-03 23:38:32 +0300

    #022 lesson :
    - used template property instead of templateUrl

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,7 +5,10 @@ import { ServerComponent } from '../server/server.component';
   selector: 'app-servers',
   standalone: true,
   imports: [ServerComponent],
-  templateUrl: './servers.component.html',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+  `,
   styleUrl: './servers.component.css',
 })
 export class ServersComponent {}
