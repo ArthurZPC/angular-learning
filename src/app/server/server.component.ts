2b79f3041151436b64379046da220ed17bfe3697
ArthurZPC <guseynoff.artur@yandex.ru>
2024-09-05 02:34:41 +0300

    #040 lesson :
    - used ngStyle directive to update a bg-color of server component
    - cleaned some styles

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,22 +1,30 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styles: [
-    `
-      p {
-        color: red;
-      }
-    `,
-  ],
+  imports: [NgStyle],
+  styles: `
+  p {
+    color:white;
+  }
+  `,
 })
 export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus(): string {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
