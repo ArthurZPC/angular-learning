15c72182d2976b0ed1d57c5e64440f2c4e61a35b
ArthurZPC <guseynoff.artur@yandex.ru>
2024-09-06 00:56:25 +0300

    #050 lesson - course project :
    - added a layout for components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './features/recipe-book/recipes/recipes.component';
+import { ShoppingListComponent } from './features/shopping-list/shopping-list/shopping-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    RecipesComponent,
+    ShoppingListComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
