db320421c4f4da9f2aaa0ed1f0f354b1fd1ac72f
ArthurZPC <guseynoff.artur@yandex.ru>
2024-09-06 23:44:58 +0300

    #055 lesson - course project :
    - added markup for recipe-list component
    - added margin-bottom to header component

--- src/app/features/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/features/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { Recipe } from '../recipe.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-list',
   standalone: true,
-  imports: [RecipeItemComponent],
+  imports: [RecipeItemComponent, CommonModule],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://yt3.ggpht.com/a/AATXAJzR5IFcMYO3TMWMxYAfceS6zUBb-vOE5vbRyg=s900-c-k-c0xffffffff-no-rj-mo'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://yt3.ggpht.com/a/AATXAJzR5IFcMYO3TMWMxYAfceS6zUBb-vOE5vbRyg=s900-c-k-c0xffffffff-no-rj-mo'
+    ),
+  ];
 }
