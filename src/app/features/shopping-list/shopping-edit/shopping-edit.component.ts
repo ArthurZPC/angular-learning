8cb676d230193b91272fe4764d8da97cbc7c5f2c
ArthurZPC <guseynoff.artur@yandex.ru>
2024-09-07 04:02:17 +0300

    #090 lesson - course project :
    - added databinding for shopping-list feature

--- src/app/features/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/features/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from '../../../shared/models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   standalone: true,
   imports: [],
   templateUrl: './shopping-edit.component.html',
-  styleUrl: './shopping-edit.component.css'
+  styleUrl: './shopping-edit.component.css',
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInputRef!: ElementRef;
+  @ViewChild('amountInput') amountInputRef!: ElementRef;
 
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  onAddItem($event: MouseEvent) {
+    $event.preventDefault();
+    const newIngredient = new Ingredient(
+      this.nameInputRef.nativeElement.value,
+      this.amountInputRef.nativeElement.value
+    );
+
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
