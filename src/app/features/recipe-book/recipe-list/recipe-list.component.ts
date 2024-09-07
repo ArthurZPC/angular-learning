118e90d3dc556e77c63ca134c278f99931a59ef9
ArthurZPC <guseynoff.artur@yandex.ru>
2024-09-07 03:43:38 +0300

    #088 lesson - course project :
    - passed needed data to recipes components

--- src/app/features/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/features/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { RecipeItemComponent } from './recipe-item/recipe-item.component';
 import { Recipe } from '../recipe.model';
 import { CommonModule } from '@angular/common';
@@ -11,6 +11,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
@@ -18,9 +19,13 @@ export class RecipeListComponent {
       'https://yt3.ggpht.com/a/AATXAJzR5IFcMYO3TMWMxYAfceS6zUBb-vOE5vbRyg=s900-c-k-c0xffffffff-no-rj-mo'
     ),
     new Recipe(
-      'A Test Recipe',
+      'Another Recipe',
       'This is simply a test',
       'https://yt3.ggpht.com/a/AATXAJzR5IFcMYO3TMWMxYAfceS6zUBb-vOE5vbRyg=s900-c-k-c0xffffffff-no-rj-mo'
     ),
   ];
+
+  onRecipeSelected(recipeEl: Recipe) {
+    this.recipeWasSelected.emit(recipeEl);
+  }
 }
