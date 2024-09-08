8c016d9bf042f939cabf4c8345758f9c6af1c0ec
ArthurZPC <guseynoff.artur@yandex.ru>
2024-09-08 22:33:30 +0300

    #109 lesson - services-and-DI :
    - created accounts service
    - updated components to use accounts service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AccountsService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, AccountComponent, NewAccountComponent, CommonModule],
+  providers: [AccountsService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active',
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive',
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown',
-    },
-  ];
+export class AppComponent implements OnInit {
+  accounts: { name: string; status: string }[] = [];
 
-  onAccountAdded(newAccount: { name: string; status: string }) {
-    this.accounts.push(newAccount);
-  }
+  constructor(private accountsService: AccountsService) {}
 
-  onStatusChanged(updateInfo: { id: number; newStatus: string }) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  ngOnInit(): void {
+    this.accounts = this.accountsService.accounts;
   }
 }
