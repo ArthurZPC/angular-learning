1f986c7c0b90e02cf355ab39e639a551c73c24e6
ArthurZPC <guseynoff.artur@yandex.ru>
2024-09-10 00:31:22 +0300

    #133 lesson - routing :
    - added route with parameter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ServersComponent } from './servers/servers.component';
 import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'servers', component: ServersComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'users/:id', component: UserComponent },
 ];
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,6 +3,7 @@ import { ServersService } from './servers.service';
 import { CommonModule } from '@angular/common';
 import { EditServerComponent } from './edit-server/edit-server.component';
 import { ServerComponent } from './server/server.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -14,9 +15,13 @@ import { ServerComponent } from './server/server.component';
 export class ServersComponent implements OnInit {
   public servers: { id: number; name: string; status: string }[] = [];
 
-  constructor(private serversService: ServersService) {}
+  constructor(private serversService: ServersService, private router: Router) {}
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    this.router.navigate(['/servers']);
   }
 }
