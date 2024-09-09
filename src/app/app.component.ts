7fe0d8a73cad45f3032d91f65d8433ea7de7ed69
ArthurZPC <guseynoff.artur@yandex.ru>
2024-09-10 00:00:36 +0300

    #127 lesson - routing :
    - added routes to the app.routes
    - updated app.component view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { UsersComponent } from './users/users.component';
 import { ServersComponent } from './servers/servers.component';
 import { HomeComponent } from './home/home.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [UsersComponent, ServersComponent, HomeComponent],
+  imports: [UsersComponent, ServersComponent, HomeComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { UsersComponent } from './users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'servers', component: ServersComponent },
+  { path: 'users', component: UsersComponent },
+];
