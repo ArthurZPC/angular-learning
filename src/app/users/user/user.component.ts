d858d512b2beab763831fd01599031a20a4db1a1
ArthurZPC <guseynoff.artur@yandex.ru>
2024-09-10 00:59:02 +0300

    #135 lesson - routing :
    - subscribed to route.params to update the data of component

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
@@ -17,5 +18,10 @@ export class UserComponent implements OnInit {
       id: Number(this.route.snapshot.paramMap.get('id')),
       name: this.route.snapshot.paramMap.get('name')!,
     };
+
+    this.route.params.subscribe((params: Params) => {
+      this.user.id = params['id'];
+      this.user.name = params['name'];
+    });
   }
 }
