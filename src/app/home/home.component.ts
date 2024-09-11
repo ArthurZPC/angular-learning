cb8f72d0da44f778fbce05c956583426b7301b99
ArthurZPC <guseynoff.artur@yandex.ru>
2024-09-12 01:26:41 +0300

    #174 lesson - observables :
    - added compete notification

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,16 +15,30 @@ export class HomeComponent implements OnInit, OnDestroy {
     //   console.log(count);
     // });
 
-    const customIntervalObservable = new Observable((observer) => {
+    const customIntervalObservable = new Observable<number>((observer) => {
       let count = 0;
       setInterval(() => {
-        observer.next(count++);
+        observer.next(count);
+
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
+
+        count++;
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe((t) =>
-      console.log(t)
-    );
+    this.firstObsSubscription = customIntervalObservable.subscribe({
+      next: (t) => console.log(t),
+      error: (e: Error) => {
+        console.log(e);
+        alert(e.message);
+      },
+      complete: () => console.log('Completed!'),
+    });
   }
 
   ngOnDestroy() {
