15723a5d58399d9ca80614af52b888237e2b22de
ArthurZPC <guseynoff.artur@yandex.ru>
2024-09-12 01:11:44 +0300

    #173 lesson - observables : - created custom observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription!: Subscription;
+
+  ngOnInit(): void {
+    // this.firstObsSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+
+    const customIntervalObservable = new Observable((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count++);
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObservable.subscribe((t) =>
+      console.log(t)
+    );
+  }
+
+  ngOnDestroy() {
+    this.firstObsSubscription.unsubscribe();
+  }
+}
