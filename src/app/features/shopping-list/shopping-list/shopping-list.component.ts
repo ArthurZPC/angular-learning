6a0af6c2b72afe40c7458b2e36124ee7b7ee20bb
ArthurZPC <guseynoff.artur@yandex.ru>
2024-09-12 02:07:16 +0300

    #180 lesson - course-project :
    - removed unused event emitter from recipe.service
    - updated ingredientsChanged event emitter to a subject

--- src/app/features/shopping-list/shopping-list/shopping-list.component.ts
+++ src/app/features/shopping-list/shopping-list/shopping-list.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ShoppingEditComponent } from '../shopping-edit/shopping-edit.component';
 import { Ingredient } from '../../../shared/models/ingredient.model';
 import { CommonModule } from '@angular/common';
 import { ShoppingListService } from '../shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
@@ -11,15 +12,21 @@ import { ShoppingListService } from '../shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css',
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients!: Ingredient[];
+  ingredientsChangedSub!: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsChanged.subscribe(
-      (ingredients: Ingredient[]) => (this.ingredients = ingredients)
-    );
+    this.ingredientsChangedSub =
+      this.shoppingListService.ingredientsChanged.subscribe(
+        (ingredients: Ingredient[]) => (this.ingredients = ingredients)
+      );
+  }
+
+  ngOnDestroy() {
+    this.ingredientsChangedSub.unsubscribe();
   }
 }
