74ea8e516908576bc85ec3203bbd1cd755ce03b7
ArthurZPC <guseynoff.artur@yandex.ru>
2024-09-12 01:58:14 +0300

    #177 lesson - observables :
    - subjects practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { UserService } from './user.service';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  private activatedSub!: Subscription;
+
+  constructor(private userService: UserService) {}
+
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(
+      (x) => (this.userActivated = x)
+    );
+  }
+}
